0dca84e8c9cd7b0fb4ef4b7bca33299637bf57b2
Eric Alt <ericallenalt@gmail.com>
2020-05-01 23:57:44 -0700

    feat: Add function to get DisProt data
    DisProt has an api to get data for a protein based on its id.
    The `show_ambiguous` and `show_obsolete` options are set to true.
    These values give the most complete data.
    This choice may later be revised to improve data quality.
    The amino acid sequence is the desired predictor data, `X`.
    The type label for each amino acid is the desired target data, `Y`.
    A helper function is constructs that sequence from the raw return data.
    Both sequences are returned as numpy unicode arrays.
    Uncharacterized amino acids are given the label `?`.

--- source_data.py
+++ source_data.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import json
+import numpy as np
 import utils
 
 
@@ -8,3 +10,25 @@ def get_ids():
     response = utils.try_get_request({'url': 'https://www.disprot.org/api/list_ids'})
     ids = json.loads(response.text)
     return ids['disprot_ids']
+
+
+def get_data(id):
+    response = utils.try_get_request({
+        'url': 'https://www.disprot.org/api/' + id,
+        'params': OrderedDict([
+            ('show_ambiguous', 'true'),
+            ('show_obsolete', 'true'),
+            ('release', '2019_09'),
+            ('format', 'json')
+        ])
+    })
+    data = json.loads(response.text)
+    X = np.array(list(data['sequence']), dtype=np.unicode_)
+    Y = get_sequence_labels(data['length'], data['disprot_consensus']['full'])
+    return X, Y
+
+def get_sequence_labels(length, consensus):
+    Y = np.array(['?']*length, dtype=np.unicode_)
+    for region in consensus:
+        Y[region['start']-1:region['end']] = region['type']
+    return Y
--- source_data_test.py
+++ source_data_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 import source_data
 
@@ -9,6 +10,28 @@ class SourceDataTest(unittest.TestCase):
         self.assertTrue(type(ids), list)
         self.assertGreater(len(ids), 0)
         self.assertTrue(all([type(id) == unicode for id in ids]))
+
+    def test_get_data(self):
+        id = 'DP00086'
+        expected_X = np.array(list(
+            'MEEPQSDPSVEPPLSQETFSDLWKLLPENNVLSPLPSQAMDDLMLSPDDIEQWFTEDPGPDEAPRM'
+            'PEAAPPVAPAPAAPTPAAPAPAPSWPLSSSVPSQKTYQGSYGFRLGFLHSGTAKSVTCTYSPALNK'
+            'MFCQLAKTCPVQLWVDSTPPPGTRVRAMAIYKQSQHMTEVVRRCPHHERCSDSDGLAPPQHLIRVE'
+            'GNLRVEYLDDRNTFRHSVVVPYEPPEVGSDCTTIHYNYMCNSSCMGGMNRRPILTIITLEDSSGNL'
+            'LGRNSFEVRVCACPGRDRRTEEENLRKKGEPHHELPPGSTKRALPNNTSSSPQPKKKPLDGEYFTL'
+            'QIRGRERFEMFRELNEALELKDAQAGKEPGGSRAHSSHLKSKKGQSTSRHKKLMFKTEGPDSD'),
+            dtype=np.unicode_)
+        expected_Y = np.array(list(
+            'TTTTTTTTTTTTTTTTTTTIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII'
+            'IIIIIIITTTTTTTTTTTTTTTTTTTTIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII'
+            'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII'
+            'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII'
+            'IIIIIIIIIIIIIIIIIIIIIIIIIIIIDDDDDDDDDDDDDDDDDDDD???????IIIIIIIIIII'
+            'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII'),
+            dtype=np.unicode_)
+        X, Y = source_data.get_data(id)
+        self.assertTrue(np.array_equal(X, expected_X))
+        self.assertTrue(np.array_equal(Y, expected_Y))
 
 
 if __name__ == '__main__':
