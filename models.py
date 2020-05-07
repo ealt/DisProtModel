4b239833df3cd0e021fa338d00af6644b53ce6ad
Eric Alt <ericallenalt@gmail.com>
2020-05-06 19:48:52 -0700

    refactor: Replace class methods with util
    Multiple class methods were created to recure through a data structure.
    A util method was created to accomplish this task.
    These custom methods were eleminated by calling the util.

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import numpy as np
 from operator import itemgetter
 from scipy import stats
 from sklearn.base import BaseEstimator, ClassifierMixin
+import utils
 
 class ModalValueClassifier(BaseEstimator,ClassifierMixin):
     def fit(self, X, Y=None):
@@ -11,17 +12,11 @@ class ModalValueClassifier(BaseEstimator,ClassifierMixin):
         if isinstance(Y, np.ndarray):
             self._mode = stats.mode(Y.flatten())[0][0]
         else:
-            self._unigram_counts = Counter()
-            self._update_unigram_counts(Y)
-            self._mode = max(self._unigram_counts.items(), key=itemgetter(1))[0]
+            unigram_counts = Counter()
+            for y in utils.flatten(Y):
+                unigram_counts[y] += 1
+            self._mode = max(unigram_counts.items(), key=itemgetter(1))[0]
         return self
-    
-    def _update_unigram_counts(self, Y):
-        if hasattr(Y, '__iter__') and type(Y) != str:
-            for y in Y:
-                self._update_unigram_counts(y)
-        else:
-            self._unigram_counts[Y] += 1
 
     def predict(self, X):
         if not hasattr(self, '_mode'):
@@ -45,16 +40,10 @@ class ModalValueClassifier(BaseEstimator,ClassifierMixin):
             raise RuntimeError("Model must be fit before calling predict")
         if Y is None:
             Y = X
-        self._correct = 0
-        self._total = 0
-        self._update_score_counts(Y)
-        return self._correct / self._total
-
-    def _update_score_counts(self, Y):
-        if hasattr(Y, '__iter__') and type(Y) != str:
-            for y in Y:
-                self._update_score_counts(y)
-        else:
-            self._total += 1
-            if Y == self._mode:
-                self._correct += 1
+        correct = 0
+        total = 0
+        for y in utils.flatten(Y):
+            total += 1
+            if y == self._mode:
+                correct += 1
+        return correct / total
