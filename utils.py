3501f9af96f1394f9644a5519bd0ba2277aebe63
Eric Alt <ericallenalt@gmail.com>
2020-05-17 22:59:21 -0700

    feat: Add util function to get value counts
    The correlation length is a helpful stat in designing sequence models.
    An ordinal autocorrleation function needs to be defined.
    This function requires value counts.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import numpy as np
 import requests
 
@@ -25,6 +26,28 @@ def get_lag_match_frequency(X, k=1):
         num_matches += n - np.count_nonzero(dx)
         num_total += n - np.count_nonzero(np.isnan(dx))
     return num_matches / num_total
+
+def get_value_counts(X, k=1):
+    head = Counter()
+    mid = Counter()
+    tail = Counter()
+    for x in X:
+        if len(x) >= 2 * k:
+            update_counts(head, x[:k])
+            update_counts(mid, x[k:-k])
+            update_counts(tail, x[-k:])
+        else:
+            update_counts(head, x[:-k])
+            update_counts(tail, x[k:])
+    head += mid
+    tail += mid
+    return head, tail
+
+def update_counts(counter, x):
+    values, value_counts = np.unique(x, return_counts=True)
+    for value, value_count in zip(values, value_counts):
+        if ~np.isnan(value):
+            counter[value] += value_count
 
 def eq(a, b):
     if not (hasattr(a, '__iter__') or type(a) == str):
--- utils_test.py
+++ utils_test.py
@@ -28,6 +28,24 @@ class UtilsTest(unittest.TestCase):
         self.assertEqual(utils.get_lag_match_frequency(X), 0.75)
         self.assertEqual(utils.get_lag_match_frequency(X, 4), 1.0)
 
+    def test_get_value_counts(self):
+        X = [
+            np.array([0, 0, 1, np.nan, np.nan, 0, 1, 1]),
+            np.array([0, 0, 0, 0, 0])
+        ]
+
+        expected_head = {0.: 7, 1.: 2}
+        expected_tail = {0.: 6, 1.: 3}
+        head, tail = utils.get_value_counts(X)
+        self.assertDictEqual(head, expected_head)
+        self.assertDictEqual(tail, expected_tail)
+
+        expected_head = {0.: 3, 1.: 1}
+        expected_tail = {0.: 2, 1.: 2}
+        head, tail = utils.get_value_counts(X, 4)
+        self.assertDictEqual(head, expected_head)
+        self.assertDictEqual(tail, expected_tail)
+
 
 if __name__ == '__main__':
     unittest.main()
